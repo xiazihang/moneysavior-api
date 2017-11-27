fb9519184816f9aa740065b17e4c96866c6d75e0
xiazihang <zhxia@thoughtworks.com>
2017-11-27 17:26:04 +0800

    [20171127][zihang] - import hsqldb & create init schema and data

---
+++ config/sql/data.sql
@@ -0,0 +1,11 @@
+--users seed data
+INSERT INTO users VALUES ('user1', 'pass1', 'email1', 'nickname1', 'phone1');
+INSERT INTO users VALUES ('user2', 'pass2', 'email2', 'nickname2', 'phone1');
+INSERT INTO users VALUES ('user3', 'pass3', 'email3', 'nickname3', 'phone1');
+INSERT INTO users VALUES ('user4', 'pass4', 'email4', 'nickname4', 'phone1');
+INSERT INTO users VALUES ('user5', 'pass5', 'email5', 'nickname5', 'phone1');
+INSERT INTO users VALUES ('user6', 'pass6', 'email6', 'nickname6', 'phone1');
+INSERT INTO users VALUES ('user7', 'pass7', 'email7', 'nickname7', 'phone1');
+INSERT INTO users VALUES ('user8', 'pass8', 'email8', 'nickname8', 'phone1');
+INSERT INTO users VALUES ('user9', 'pass9', 'email9', 'nickname9', 'phone1');
+INSERT INTO users VALUES ('user10', 'pass10', 'email10', 'nickname10', 'phone1');
